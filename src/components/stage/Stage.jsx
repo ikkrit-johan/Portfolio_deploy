f127f78e5cfe6425d3cf0374fa8893e88b8fce28
ikkrit-johan <bouhbouh379@gmail.com>
2022-05-21 20:11:05 +0200

    m

--- src/components/stage/Stage.jsx
+++ src/components/stage/Stage.jsx
@@ -17,6 +17,45 @@ const Stage = () => {
 
                     <div className="stage__intro--present">
                         <h4>Mes stages en entreprise ont etait fait à l'EIRL Sébastien Ducly</h4>
+
+                        <div className="stage__intro--para">
+                            <p>- Mettre en place environnement de travail logiciels et matériels : </p>
+                            <p>- Mon tuteur ma confier un pc portable à nettoyer pour en faire mon poste de travail</p>
+                            <p>- Remplacement du processeur , upgrade de la ram, reintalation complete de system (windows 10), antivirus...</p>
+                            <p>- Installer l'environnement de developpement (vsc, Symfony, React, PHP, Bracket, Slack ...)</p>
+                            <p>- Dans le respect des normes etablie par mon tuteur</p>
+                            <p>- Installation est configuration de la base de données (Wamp, PHP admin)</p>
+                            <p>- Mise en place de sauvegarde system externe, versioning du projet avec GIT/Bracket</p>
+                            <p>- La gestion du materiel etait sous ma responsabilité, ainsi que leurs securité</p>
+                            <p>- Quelques interventions, remplacement d'une carte graphique ( MSI 3070)</p>
+                            <p>- Installer et configuration d'un eco system VR (Oculus S)</p>
+                            
+
+                            <h4>Client</h4>
+                            <p>- Mise en relation avec le client</p>
+                            <p>- La cliente est une illustratrice</p>
+                            <p>- Analyse de la demande client.</p>
+                            <p>- Site pour mettre en avant ces oeuvres</p>
+                            <p>- Analyse des moyens a mettre en place.</p>
+                            <p>- Maquette fonctionnel du projet sous Figma à partir de la demande et du style rechercher</p>
+
+                            <p>- Analyse des technologie employer</p>
+                            <p>- le projet est developper avec le Framework Symfony</p>
+                            <p>- Prise en charge des commentaires suite au PR.</p>
+                            <p>- Analyse des délais</p>
+
+                            <h4>Equipe</h4>
+                            
+                            <p>- Travail en équipe suivant la méthode Agile, en céréromonies SCRUM .</p>
+                            <p>- Mise en place d’un Kanban ( Trello ).</p>
+                            <p>- Gestion des versions du projet avec Git / Bitbucket.</p>
+
+                            <h4>Formation</h4>
+                            <p>- Formation au fonctionnement des Framework.</p>
+                            <p>- Formation a la POO PHP, JavaScript, SASS...</p>
+                            <p>- Formation FIGMA.</p>
+                            
+                        </div>
                         
                         <div className="stage__icon">
                             <div className="stage__illu">
